f9d21a982784465d23013998d9131e69c26fc768
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-01 18:41:11 -0300

    define a estrutura do projeto em modulos

---
+++ src/modules/user/models/User.ts
@@ -0,0 +1,32 @@
+import { compare } from "bcryptjs";
+
+class User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  tel: string;
+  role: string;
+
+  constructor(
+    id: number,
+    name: string,
+    email: string,
+    tel: string,
+    password: string,
+    role: string,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.tel = tel;
+    this.password = password;
+    this.role = role;
+  }
+
+  public async checkPassword(password: string): Promise<boolean> {
+    return compare(password, this.password);
+  }
+}
+
+export default User;
