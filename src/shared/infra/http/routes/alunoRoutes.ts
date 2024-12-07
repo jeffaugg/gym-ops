bb232f864118a32a6274c781b5722664ca2479ae
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-07 19:09:44 -0300

    adiciona rotas para o gerenciamento de alunos com operações CRUD

---
+++ src/shared/infra/http/routes/alunoRoutes.ts
@@ -0,0 +1,16 @@
+import { container } from "tsyringe";
+import { AlunoController } from "../../../../modules/alunos/controller/AlunoController";
+import { Router } from "express";
+
+const alunoRoutes = Router();
+const alunoController = container.resolve(AlunoController);
+
+alunoRoutes.post("/", alunoController.create);
+alunoRoutes.get("/", alunoController.list);
+alunoRoutes.put("/:id", alunoController.update);
+alunoRoutes.delete("/:id", alunoController.delete);
+alunoRoutes.get("/:id", alunoController.findById);
+alunoRoutes.get("/email/:email", alunoController.findByEmail);
+alunoRoutes.get("/cpf/:cpf", alunoController.findByCpf);
+
+export default alunoRoutes;
