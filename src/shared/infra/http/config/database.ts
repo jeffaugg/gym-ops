8576bcb64b91dc673dce2364754ed3ae43720a25
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-07 19:09:37 -0300

    adiciona registro da instância do Knex no container de injeção de dependência

--- src/shared/infra/http/config/database.ts
+++ src/shared/infra/http/config/database.ts
@@ -1,6 +1,8 @@
-import knex from "knex";
+import knex, { Knex } from "knex";
 import config from "../../../../../knexfile";
+import { container } from "tsyringe";
 // Inicializa o Knex com o ambiente de desenvolvimento
 const db = knex(config.development);
+container.registerInstance<Knex>("Database", db);
 
 export default db;
