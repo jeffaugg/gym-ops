4d0a213dc1570caafa561bf5362f628e9505fddb
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-12 13:52:07 -0300

    feat: atualiza método create e adiciona findByAvaliacaoId na classe FotosRepository

--- Back/src/modules/avaliacoes/repository/FotosRepository.ts
+++ Back/src/modules/avaliacoes/repository/FotosRepository.ts
@@ -5,12 +5,22 @@ import { inject, injectable } from "tsyringe";
 export class FotosRepository {
   constructor(@inject("Database") private db: Knex) {}
 
-  async create(url: string, avaliacao_id: number): Promise<string[]> {
+  async create(url: string, avaliacao_id: number): Promise<string> {
     const query = `
-    INSERT INTO fotos (avaliacao_id, url) 
+    INSERT INTO fotos_avaliacoes (avaliacao_id, foto_path) 
     VALUES (? ,?)
-    RETURNING url;`;
+    RETURNING foto_path;`;
 
-    return await this.db.raw<string[]>(query, [avaliacao_id, url]);
+    const result = await this.db.raw(query, [avaliacao_id, url]);
+    return result.rows[0].foto_path;
+  }
+
+  async findByAvaliacaoId(avaliacao_id: number): Promise<string[]> {
+    const query = `
+    SELECT foto_path FROM fotos_avaliacoes WHERE avaliacao_id = ?;  
+    `;
+    const result = await this.db.raw(query, [avaliacao_id]);
+
+    return result.rows.map((foto: any) => foto.foto_path);
   }
 }
