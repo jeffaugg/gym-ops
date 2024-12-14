a052b9ce270f7568306f47da86650f516c5e85a8
WendelRodriguesz <wendeldev2010@gmail.com>
2024-12-14 14:10:01 -0300

    fit: Modificação da topbar e resolvendo conflitos do students

--- Front/gym-ops/src/pages/Students/Students.jsx
+++ Front/gym-ops/src/pages/Students/Students.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Sidebar from "../../components/Admin/Sidebar/Sidebar";
+import Topbar from "../../components/Topbar/Topbar";
 import "./Students.css";
-import Dashboard from "../../components/Admin/Home/Dashboard/Dashboard";
 
 function Students() {
   return (
     <div className="students">
       <Sidebar />
+      <div className="main-content">
+        <Topbar />
+        <div className="content-container">
+          <header className="header">
+            <h1>Alunos matriculados</h1>
+          </header>
+        </div>
+      </div>
     </div>
   );
 }
