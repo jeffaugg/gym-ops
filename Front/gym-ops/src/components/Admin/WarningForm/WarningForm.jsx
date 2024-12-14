79c9c552b165d280b95759780c4f3747da9fdb5d
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2024-12-14 13:11:15 -0300

    feat: criacao do componente dos botoes de enviar e cancelar, componentizazao da tela de usuarios

--- Front/gym-ops/src/components/Admin/WarningForm/WarningForm.jsx
+++ Front/gym-ops/src/components/Admin/WarningForm/WarningForm.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./WarningForm.css";
+import ButtonCancel from "../../ButtonCancel/ButtonCancel";
+import ButtonSend from "../../ButtonSend/ButtonSend";
+import InputFieldForm from "../../InputFieldForm/InputFieldForm";
 
 export default function WarningForm() {
-    return (
-        <div className="mensagem-form">
-             <label>
-              Título*
-              <input type="text" placeholder="Digite o título" required />
-            </label>
-            <label>
-              Mensagem*
-              <textarea placeholder="Digite sua mensagem" required></textarea>
-            </label>
-            <label>
-              Enviar para
-              <select>
-                <option value="todos">Todos</option>
-                <option value="instrutores">Somente Instrutores</option>
-              </select>
-            </label>
-            <div className="form-actions">
-              <button type="submit" className="btn enviar">Enviar</button>
-              <button type="button" className="btn cancelar">Cancelar</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mensagem-form">
+      <InputFieldForm label="Título*" type="text" placeholder={"Digite o título"}/>
+      <label>
+        Mensagem*
+        <textarea placeholder="Digite sua mensagem" required></textarea>
+      </label>
+      <label>
+        Enviar para
+        <select>
+          <option value="todos">Todos</option>
+          <option value="instrutores">Somente Instrutores</option>
+        </select>
+      </label>
+      <div className="form-actions">
+        <ButtonSend />
+        <ButtonCancel />
+      </div>
+    </div>
+  );
 }
--- Front/gym-ops/src/pages/Students/Students.jsx
+++ Front/gym-ops/src/pages/Students/Students.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Sidebar from "../../components/Admin/Sidebar/Sidebar";
 import "./Students.css";
+import StudentsForm from "../../components/Admin/StudentsForm/StudentsForm";
+import StudentsTable from "../../components/StudentsTable/StudentsTable";
 
 function Students() {
   return (
@@ -8,106 +10,13 @@ function Students() {
       <Sidebar />
       <div className="students-content">
         <header className="students-header">
-          <h1>Editar aluno</h1>
+          <h1>Alunos</h1>
         </header>
 
-        <div className="students-form">
-          <form>
-            <div className="form-group">
-              <label>
-                Nome*
-                <input type="text" placeholder="Digite o nome" required />
-              </label>
-              <label>
-                Data de nascimento*
-                <input type="date" required />
-              </label>
-            </div>
+        <StudentsForm />
+        <StudentsTable />
 
-            <div className="form-group">
-              <label>
-                CPF*
-                <input type="text" placeholder="Digite o CPF" required />
-              </label>
-              <label>
-                Gênero*
-                <select required>
-                  <option value="">Selecione</option>
-                  <option value="masculino">Masculino</option>
-                  <option value="feminino">Feminino</option>
-                  <option value="outro">Outro</option>
-                </select>
-              </label>
-            </div>
-
-            <div className="form-group">
-              <label>
-                Planos*
-                <select required>
-                  <option value="">Selecione</option>
-                  <option value="mensal">Mensal</option>
-                  <option value="anual">Anual</option>
-                </select>
-              </label>
-              <label>
-                Telefone*
-                <input type="text" placeholder="Digite o telefone" required />
-              </label>
-            </div>
-
-            <div className="form-group">
-              <label>
-                Email*
-                <input type="email" placeholder="Digite o email" required />
-              </label>
-              <label>
-                Observações de saúde
-                <input type="text" placeholder="Digite as observações" />
-              </label>
-            </div>
-
-            <div className="form-actions">
-              <button type="submit" className="btn salvar">Salvar</button>
-              <button type="button" className="btn cancelar">Cancelar</button>
-            </div>
-          </form>
-        </div>
-
-        <div className="students-list">
-          <table>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>CPF</th>
-                <th>Telefone</th>
-                <th>Status</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Ana Almeida</td>
-                <td>123.456.789-10</td>
-                <td>(88)991234567</td>
-                <td>Inativo</td>
-                <td>
-                  <button className="btn edit">✏️</button>
-                  <button className="btn delete">❌</button>
-                </td>
-              </tr>
-              <tr>
-                <td>Pedro Siqueira</td>
-                <td>123.456.789-10</td>
-                <td>(88)991234567</td>
-                <td>Ativo</td>
-                <td>
-                  <button className="btn edit">✏️</button>
-                  <button className="btn delete">❌</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+  
       </div>
     </div>
   );
