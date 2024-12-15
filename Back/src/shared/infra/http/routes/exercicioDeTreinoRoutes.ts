57eaad263ed45bb4c7bbe62de02554c0ca7b02aa
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-15 18:25:39 -0300

    feat: renomeia rota de exercícios de treino e adiciona rota para mensagens

--- Back/src/shared/infra/http/routes/exercicioDeTreinoRoutes.ts
+++ Back/src/shared/infra/http/routes/exercicioDeTreinoRoutes.ts
@@ -10,6 +10,6 @@ exercicioDeTreinoRoutes.get("/", exercicioDeTreinoController.list);
 exercicioDeTreinoRoutes.put("/:id", exercicioDeTreinoController.update);
 exercicioDeTreinoRoutes.delete("/:id", exercicioDeTreinoController.delete);
 exercicioDeTreinoRoutes.get("/:id", exercicioDeTreinoController.findById);
-exercicioDeTreinoRoutes.get("/treino/:treino_id", exercicioDeTreinoController.findByTreinoId);
+exercicioDeTreinoRoutes.get("/workouts/:treino_id", exercicioDeTreinoController.findByTreinoId);
 
 export default exercicioDeTreinoRoutes;
