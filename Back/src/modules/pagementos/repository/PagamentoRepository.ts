1ba54fbd4af4b93b8d5ad3840a89c50facd4da2b
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-16 20:29:41 -0300

    feat: cria repositório de pagamentos com operações CRUD

---
+++ Back/src/modules/pagementos/repository/PagamentoRepository.ts
@@ -0,0 +1,62 @@
+import { Knex } from "knex";
+import { inject, injectable } from "tsyringe";
+import { PagamentoSchema } from "../dto/PagamentoSchema";
+import { z } from "zod";
+import { Pagamento } from "../models/Pagamento";
+
+@injectable()
+export class PagamentoRepository {
+  constructor(@inject("Database") private db: Knex) {}
+
+  async create(data: z.infer<typeof PagamentoSchema>): Promise<Pagamento> {
+    const query = `
+      INSERT INTO pagamentos (id_aluno, id_plano, status, payment, expiration_date)
+      VALUES (?, ?, ?, ?, ?)
+      RETURNING id, id_aluno, id_plano, status, payment, payment_date, expiration_date;
+    `;
+
+    const result = await this.db.raw(query, [
+      data.id_aluno,
+      data.id_plano,
+      true,
+      data.payment,
+      data.expiration_date,
+    ]);
+
+    return Pagamento.fromDatabase(result.rows[0]);
+  }
+
+  async list(): Promise<Pagamento[]> {
+    const query = "SELECT * FROM pagamentos";
+    const result = await this.db.raw(query);
+
+    return result.rows.map((pagamentoData: any) =>
+      Pagamento.fromDatabase(pagamentoData),
+    );
+  }
+
+  async findById(id: number): Promise<Pagamento | null> {
+    const query = "SELECT * FROM pagamentos WHERE id = ?";
+    const result = await this.db.raw(query, [id]);
+
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    return Pagamento.fromDatabase(result.rows[0]);
+  }
+
+  async findByAlunoId(id: number): Promise<Pagamento[]> {
+    const query = "SELECT * FROM pagamentos WHERE id_aluno = ?";
+    const result = await this.db.raw(query, [id]);
+
+    return result.rows.map((pagamentoData: any) =>
+      Pagamento.fromDatabase(pagamentoData),
+    );
+  }
+
+  async delete(id: number): Promise<void> {
+    const query = "DELETE FROM pagamentos WHERE id = ?";
+    await this.db.raw(query, [id]);
+  }
+}
