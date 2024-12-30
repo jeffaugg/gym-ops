79aa9794e5d8ba041939cbb6c9b00d19c32b9b56
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-30 19:55:41 -0300

    feat: adicionar validação de administrador e incluir adm_id nas operações do AlunoService

--- Back/src/modules/alunos/service/AlunoService.ts
+++ Back/src/modules/alunos/service/AlunoService.ts
@@ -4,6 +4,7 @@ import { AlunoSchema } from "../dto/AlunoSchema";
 import { z } from "zod";
 import AppError from "../../../shared/errors/AppError";
 import { PlanoRepository } from "../../planos/repository/PlanoRepository";
+import UserRepository from "../../user/repositories/UserRepository";
 
 @injectable()
 export class AlunoService {
@@ -12,22 +13,33 @@ export class AlunoService {
     private alunoRepository: AlunoRepository,
     @inject(PlanoRepository)
     private planoRepository: PlanoRepository,
+    @inject(UserRepository)
+    private userRepository: UserRepository,
   ) {}
 
-  async create(data: z.infer<typeof AlunoSchema>) {
-    const userByEmail = await this.alunoRepository.findByEmail(data.email);
+  async create(data: z.infer<typeof AlunoSchema>, adm_id: number) {
+    const admById = await this.userRepository.findById(adm_id);
+
+    if (!admById || admById.role != "ADM") {
+      throw new AppError("Administrador inválido", 404);
+    }
+
+    const userByEmail = await this.alunoRepository.findByEmail(
+      data.email,
+      adm_id,
+    );
 
     if (userByEmail) {
       throw new AppError("Email já cadastrado", 409);
     }
 
-    const userByCpf = await this.alunoRepository.findByCpf(data.cpf);
+    const userByCpf = await this.alunoRepository.findByCpf(data.cpf, adm_id);
 
     if (userByCpf) {
       throw new AppError("CPF já cadastrado", 409);
     }
 
-    const plano = await this.planoRepository.findById(data.plan_id);
+    const plano = await this.planoRepository.findById(data.plan_id, adm_id);
 
     if (!plano) {
       throw new AppError("Plano não encontrado", 404);
@@ -37,28 +49,33 @@ export class AlunoService {
       data.health_notes = null;
     }
 
-    return await this.alunoRepository.create(data);
+    const alunoData = { ...data, adm_id };
+
+    return await this.alunoRepository.create(alunoData);
   }
 
-  async list() {
-    return this.alunoRepository.list();
+  async list(adm_id: number) {
+    return this.alunoRepository.list(adm_id);
   }
 
-  async update(id: number, data: z.infer<typeof AlunoSchema>) {
-    const user = await this.alunoRepository.findById(id);
+  async update(id: number, adm_id: number, data: z.infer<typeof AlunoSchema>) {
+    const user = await this.alunoRepository.findById(id, adm_id);
 
     if (!user) {
       throw new AppError("Aluno não encontrado", 404);
     }
 
-    const plano = await this.planoRepository.findById(data.plan_id);
+    const plano = await this.planoRepository.findById(data.plan_id, adm_id);
 
-    const userByCpf = await this.alunoRepository.findByCpf(data.cpf);
+    const userByCpf = await this.alunoRepository.findByCpf(data.cpf, adm_id);
     if (user.cpf != data.cpf && userByCpf) {
       throw new AppError("CPF já cadastrado", 409);
     }
 
-    const userByEmail = await this.alunoRepository.findByEmail(data.email);
+    const userByEmail = await this.alunoRepository.findByEmail(
+      data.email,
+      adm_id,
+    );
 
     if (user.email != data.email && userByEmail) {
       throw new AppError("Email já cadastrado", 409);
@@ -68,11 +85,11 @@ export class AlunoService {
       throw new AppError("Plano não encontrado", 404);
     }
 
-    return await this.alunoRepository.update(id, data);
+    return await this.alunoRepository.update(id, adm_id, data);
   }
 
-  async findById(id: number) {
-    const user = await this.alunoRepository.findById(id);
+  async findById(id: number, adm_id: number) {
+    const user = await this.alunoRepository.findById(id, adm_id);
 
     if (!user) {
       throw new AppError("Aluno não encontrado", 404);
@@ -81,18 +98,18 @@ export class AlunoService {
     return user;
   }
 
-  async delete(id: number) {
-    const user = await this.alunoRepository.findById(id);
+  async delete(id: number, adm_id: number) {
+    const user = await this.alunoRepository.findById(id, adm_id);
 
     if (!user) {
       throw new AppError("Aluno não encontrado", 404);
     }
 
-    return await this.alunoRepository.delete(id);
+    return await this.alunoRepository.delete(id, adm_id);
   }
 
-  async findByCpf(cpf: string) {
-    const user = await this.alunoRepository.findByCpf(cpf);
+  async findByCpf(cpf: string, adm_id: number) {
+    const user = await this.alunoRepository.findByCpf(cpf, adm_id);
 
     if (!user) {
       throw new AppError("Aluno não encontrado", 404);
@@ -101,8 +118,8 @@ export class AlunoService {
     return user;
   }
 
-  async findByEmail(email: string) {
-    const user = await this.alunoRepository.findByEmail(email);
+  async findByEmail(email: string, adm_id: number) {
+    const user = await this.alunoRepository.findByEmail(email, adm_id);
 
     if (!user) {
       throw new AppError("Aluno não encontrado", 404);
