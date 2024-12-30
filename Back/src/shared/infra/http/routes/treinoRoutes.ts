dc2cca3165a7fe004925f0a4bee3f946cc993bdd
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-30 20:03:59 -0300

    feat: adicionar middleware de autenticação nas rotas de treino

--- Back/src/shared/infra/http/routes/treinoRoutes.ts
+++ Back/src/shared/infra/http/routes/treinoRoutes.ts
@@ -1,16 +1,17 @@
 import { container } from "tsyringe";
 import { TreinoController } from "../../../../modules/treinos/controller/TreinoController";
 import { Router } from "express";
+import isAuth from "../middleware/isAuth";
 
 const treinoRoutes = Router();
 const treinoController = container.resolve(TreinoController);
 
-treinoRoutes.post("/", treinoController.create);
-treinoRoutes.get("/", treinoController.list);
-treinoRoutes.put("/:id", treinoController.update);
-treinoRoutes.delete("/:id", treinoController.delete);
-treinoRoutes.get("/:id", treinoController.findById);
-treinoRoutes.get("/name/:name", treinoController.findByName);
-treinoRoutes.post("/adicionarExercicio", treinoController.findByName);
+treinoRoutes.post("/", isAuth, treinoController.create);
+treinoRoutes.get("/", isAuth, treinoController.list);
+treinoRoutes.put("/:id", isAuth, treinoController.update);
+treinoRoutes.delete("/:id", isAuth, treinoController.delete);
+treinoRoutes.get("/:id", isAuth, treinoController.findById);
+treinoRoutes.get("/name/:name", isAuth, treinoController.findByName);
+treinoRoutes.post("/adicionarExercicio", isAuth, treinoController.findByName);
 
 export default treinoRoutes;
