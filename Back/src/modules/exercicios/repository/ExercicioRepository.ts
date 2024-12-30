8e99b1b18610fd769006214b9717d6732ee47a74
jeffaugg <jefersonaugusto@alu.ufc.br>
2024-12-30 19:56:53 -0300

    feat: incluir adm_id nas operações do ExercicioRepository

--- Back/src/modules/exercicios/repository/ExercicioRepository.ts
+++ Back/src/modules/exercicios/repository/ExercicioRepository.ts
@@ -8,31 +8,38 @@ import { Exercicio } from "../models/Exercicio";
 export class ExercicioRepository {
   constructor(@inject("Database") private db: Knex) {}
 
-  async create(data: z.infer<typeof ExercicioSchema>): Promise<Exercicio> {
+  async create(
+    data: z.infer<typeof ExercicioSchema> & {
+      adm_id: number;
+    },
+  ): Promise<Exercicio> {
     const query = `
-      INSERT INTO exercicios (name, muscles)
-      VALUES (?, ?)
-      RETURNING id, name, muscles;
+      INSERT INTO exercicios (name, muscles, adm_id)
+      VALUES (?, ?, ?)
+      RETURNING id, name, muscles, adm_id;
     `;
 
     const result = await this.db.raw(query, [
       data.name,
-      data.muscles
+      data.muscles,
+      data.adm_id,
     ]);
 
-    return result.rows[0] as Exercicio;
+    return Exercicio.fromDatabase(result.rows[0]);
   }
 
-  async list(): Promise<Exercicio[]> {
-    const query = "SELECT * FROM exercicios";
-    const result = await this.db.raw(query);
+  async list(adm_id: number): Promise<Exercicio[]> {
+    const query = "SELECT * FROM exercicios WHERE adm_id = ?";
+    const result = await this.db.raw(query, [adm_id]);
 
-    return result.rows.map((ExercicioData: any) => Exercicio.fromDatabase(ExercicioData));
+    return result.rows.map((ExercicioData: any) =>
+      Exercicio.fromDatabase(ExercicioData),
+    );
   }
 
-  async findById(id: number): Promise<Exercicio | null> {
-    const query = "SELECT * FROM exercicios WHERE id = ?";
-    const result = await this.db.raw(query, [id]);
+  async findById(id: number, adm_id: number): Promise<Exercicio | null> {
+    const query = "SELECT * FROM exercicios WHERE id = ? AND adm_id = ?";
+    const result = await this.db.raw(query, [id, adm_id]);
 
     if (result.rows.length === 0) {
       return null;
@@ -41,34 +48,38 @@ export class ExercicioRepository {
     return Exercicio.fromDatabase(result.rows[0]);
   }
 
-  async findByName(name: string): Promise<Exercicio | null> {
-    const query = "SELECT * FROM exercicios WHERE name = ?";
-    const result = await this.db.raw(query, [name]);
+  async findByName(name: string, adm_id: number): Promise<Exercicio | null> {
+    const query = "SELECT * FROM exercicios WHERE name = ? AND adm_id = ?";
+    const result = await this.db.raw(query, [name, adm_id]);
 
     if (result.rows.length === 0) {
       return null;
     }
-    const ExercicioData = result.rows[0];
-    return Exercicio.fromDatabase(ExercicioData);
+    return Exercicio.fromDatabase(result.rows[0]);
   }
 
-  async update(id: number, data: z.infer<typeof ExercicioSchema>): Promise<Exercicio> {
+  async update(
+    id: number,
+    adm_id: number,
+    data: z.infer<typeof ExercicioSchema>,
+  ): Promise<Exercicio> {
     const query = `
     UPDATE exercicios SET name = ?, muscles = ? 
-    WHERE id = ? 
+    WHERE id = ? AND adm_id = ? 
     RETURNING id, name, muscles `;
 
     const result = await this.db.raw(query, [
       data.name,
       data.muscles,
-      id
+      id,
+      adm_id,
     ]);
 
-    return result.rows[0] as Exercicio;
+    return Exercicio.fromDatabase(result.rows[0]);
   }
 
-  async delete(id: number): Promise<void> {
-    const query = "DELETE FROM exercicios WHERE id = ?";
-    await this.db.raw(query, [id]);
+  async delete(id: number, adm_id: number): Promise<void> {
+    const query = "DELETE FROM exercicios WHERE id = ? AND adm_id = ?";
+    await this.db.raw(query, [id, adm_id]);
   }
 }
