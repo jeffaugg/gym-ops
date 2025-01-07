64e34e7de0296414fcc76518e3b1e715ee561205
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-07 12:12:20 -0300

    feat: integrar serviço de obtenção de emails ao processo de criação de mensagens

--- Back/src/modules/mensagens/service/MensagemService.ts
+++ Back/src/modules/mensagens/service/MensagemService.ts
@@ -4,15 +4,26 @@ import { z } from "zod";
 import { MensagemSchema } from "../dto/MensagemSchema";
 import UserRepository from "../../user/repositories/UserRepository";
 import AppError from "../../../shared/errors/AppError";
+import { AlunoRepository } from "../../alunos/repository/AlunoRepository";
+import { enqueueEmails } from "./EnqueueEmailsService";
+import { GetEmailsByRecipientTypeService } from "./GetEmailsByRecipientTypeService";
 
 @injectable()
 export class MensagemService {
+  private getEmailsByRecipinetTypeService: GetEmailsByRecipientTypeService;
   constructor(
     @inject(MensagemRepository)
     private mensagemRepository: MensagemRepository,
     @inject(UserRepository)
     private userRepository: UserRepository,
-  ) {}
+    @inject(AlunoRepository)
+    private alunoRepository: AlunoRepository,
+  ) {
+    this.getEmailsByRecipinetTypeService = new GetEmailsByRecipientTypeService(
+      this.alunoRepository,
+      this.userRepository,
+    );
+  }
 
   async create(data: z.infer<typeof MensagemSchema>, userId: number) {
     const user = await this.userRepository.findById(userId);
@@ -20,6 +31,13 @@ export class MensagemService {
     if (user.role !== "ADM") {
       throw new AppError("Usuário não autorizado", 401);
     }
+
+    const emails = await this.getEmailsByRecipinetTypeService.get(
+      data.recipient_type,
+      userId,
+    );
+
+    await enqueueEmails(emails, data.title, data.body);
 
     return await this.mensagemRepository.create(data, userId);
   }
