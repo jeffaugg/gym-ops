6075c7089d4be02b4f54765b8262d55eb3b80885
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-01-12 17:38:44 -0300

    feat: adicionar controlador de presença com métodos para criar, buscar e deletar registros

---
+++ Back/src/modules/presenca/controller/PresencaController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { PresencaService } from "../service/PresencaService";
+import { container, injectable } from "tsyringe";
+
+@injectable()
+export class PresencaController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const adm_id = req.user.adm_id;
+    const presencaService = container.resolve(PresencaService);
+
+    const presenca = await presencaService.create(Number(id), adm_id);
+    return res.status(201).json(presenca);
+  }
+
+  async findByUser(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const adm_id = req.user.adm_id;
+
+    const presencaService = container.resolve(PresencaService);
+
+    const presencas = await presencaService.getFindByAlunoId(
+      Number(id),
+      adm_id,
+    );
+    return res.status(200).json(presencas);
+  }
+
+  async delele(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const adm_id = req.user.adm_id;
+
+    const presencaService = container.resolve(PresencaService);
+
+    await presencaService.delete(Number(id), adm_id);
+    return res.status(204).json();
+  }
+}
