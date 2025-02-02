53cac0dea31fcf5f77bd5b7f14cf576299752771
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2025-02-02 16:46:24 -0300

    feat: adiciona botão para criar novos avisos com modal

--- Front/src/pages/Admin/Warnings/AdminWarnings.jsx
+++ Front/src/pages/Admin/Warnings/AdminWarnings.jsx
@@ -1,13 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./AdminWarnings.css";
 import WarningForm from "../../../components/Admin/WarningForm/WarningForm";
 import WarningTable from "../../../components/Admin/WarningTable/WarningTable";
 import Layout from "../../../components/Admin/LayoutPages/Layout";
+import Modal from "../../../components/Modal/Modal";
 import api from "../../../api";
 import { toast } from "react-toastify";
 
 function AdminWarnings() {
-  const [warnings, setWarnings] = useState([]); 
+  const [warnings, setWarnings] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
 
   const fetchWarnings = async () => {
@@ -19,6 +21,14 @@ function AdminWarnings() {
       console.error("Erro ao buscar as mensagens:", error);
       toast.error("Erro ao buscar as mensagens.");
     }
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
@@ -33,10 +43,20 @@ function AdminWarnings() {
       <div className="warnings-content">
         <header className="warnings-header">
           <h1>Avisos</h1>
+          <button onClick={handleOpenModal} className="btn add-warning">
+            Novo Aviso
+          </button>
         </header>
-
-        <WarningForm onWarningCreated={fetchWarnings}/>
         <WarningTable warnings={warnings} />
+      
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <WarningForm
+            onWarningCreated={() => {
+              fetchWarnings();
+              handleCloseModal();
+            }}
+          />
+        </Modal>
       </div>
     </Layout>
   );
