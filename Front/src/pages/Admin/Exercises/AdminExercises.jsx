6a293664127a6f2782dd5275006d6c74dbb08551
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2025-02-02 17:51:31 -0300

    feat: adiciona modal para adicionar e editar exercícios com botão estilizado

--- Front/src/pages/Admin/Exercises/AdminExercises.jsx
+++ Front/src/pages/Admin/Exercises/AdminExercises.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../../components/Admin/LayoutPages/Layout";
 import ExercisesForm from "../../../components/Admin/ExercisesForm/ExercisesForm";
 import ExercisesTable from "../../../components/Admin/ExercisesTable/ExercisesTable";
+import Modal from "../../../components/Modal/Modal"; 
 import api from "../../../api";
 import { toast } from "react-toastify";
 import "./AdminExercises.css";
 
 function AdminExercises() {
-  const [exercises, setExercises] = useState([]); 
-  const [selectedExercise, setSelectedExercise] = useState(null); 
-
+  const [exercises, setExercises] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
   const fetchExercises = async () => {
     try {
       const response = await api.get("/exercises");
@@ -25,25 +26,48 @@ function AdminExercises() {
     fetchExercises();
   }, []);
 
+  const handleOpenModal = () => {
+    setSelectedExercise(null); 
+    setIsModalOpen(true);
+  };
+
+  const handleEditExercise = (exercise) => {
+    setSelectedExercise(exercise);
+    setIsModalOpen(true); 
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedExercise(null);
+  };
+
   return (
     <Layout>
       <div className="exercises-content">
         <header className="exercises-header">
           <h1>Exercícios</h1>
+          <button onClick={handleOpenModal} className="btn add-exercise">
+            Adicionar Exercício
+          </button>
         </header>
-
-        <ExercisesForm
-          onExerciseCreated={fetchExercises}
-          selectedExercise={selectedExercise}
-          setSelectedExercise={setSelectedExercise}
-        />
 
         <ExercisesTable
           exercises={exercises}
           onExerciseDeleted={fetchExercises}
-          setSelectedExercise={setSelectedExercise}
+          setSelectedExercise={handleEditExercise}
           selectedExercise={selectedExercise}
         />
+
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <ExercisesForm
+            onExerciseCreated={() => {
+              fetchExercises();
+              handleCloseModal();
+            }}
+            selectedExercise={selectedExercise}
+            setSelectedExercise={setSelectedExercise}
+          />
+        </Modal>
       </div>
     </Layout>
   );
