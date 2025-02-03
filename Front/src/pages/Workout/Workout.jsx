28e5411dd12dc4eea5a1b8b563062528a7a50dd1
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2025-02-03 09:25:01 -0300

    feat: adiciona página de treinos com listagem e modal para criação/edição

---
+++ Front/src/pages/Workout/Workout.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Admin/LayoutPages/Layout";
+import WorkoutForm from "../../components/WorkoutForm/WorkoutForm";
+import WorkoutTable from "../../components/WorkoutTable/WorkoutTable";
+import Modal from "../../components/Modal/Modal";
+import api from "../../api";
+import { toast } from "react-toastify";
+import "./Workout.css";
+
+function Workout() {
+    const [workouts, setWorkouts] = useState([]);
+    const [selectedWorkout, setSelectedWorkout] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const fetchWorkouts = async () => {
+        try {
+            const response = await api.get("/workouts");
+            const sortedWorkouts = response.data.sort((a, b) => b.id - a.id);
+            setWorkouts(sortedWorkouts);
+        } catch (error) {
+            console.error("Erro ao buscar os treinos:", error);
+            toast.error("Erro ao buscar os treinos.");
+        }
+    };
+
+    useEffect(() => {
+        fetchWorkouts();
+    }, []);
+
+    const handleOpenModal = () => {
+        setSelectedWorkout(null); // Reseta o formulário
+        setIsModalOpen(true);
+    };
+
+    const handleEditWorkout = (workout) => {
+        setSelectedWorkout(workout);
+        setIsModalOpen(true); // Abre o modal para edição
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setSelectedWorkout(null);
+    };
+
+    return (
+        <Layout>
+            <div className="workout-content">
+                <header className="workout-header">
+                    <h1>Treinos</h1>
+                    <button onClick={handleOpenModal} className="btn add-workout">
+                        Adicionar Treino
+                    </button>
+                </header>
+
+                <WorkoutTable
+                    workouts={workouts}
+                    onWorkoutDeleted={fetchWorkouts}
+                    setSelectedWorkout={handleEditWorkout}
+                    selectedWorkout={selectedWorkout}
+                />
+
+                <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                    <WorkoutForm
+                        onWorkoutCreated={() => {
+                            fetchWorkouts();
+                            handleCloseModal();
+                        }}
+                        selectedWorkout={selectedWorkout}
+                        setSelectedWorkout={setSelectedWorkout}
+                    />
+                </Modal>
+            </div>
+        </Layout>
+    );
+}
+
+export default Workout;
