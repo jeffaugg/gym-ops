1620c717fa806c9ca19dc1d9e418fdcf3a8c2891
WendelRodriguesz <wendeldev2010@gmail.com>
2025-02-17 17:56:53 -0300

    feat: ajusta lógica de busca de avisos para exibir mensagem ao não haver mais itens

--- Front/src/pages/Admin/Warnings/AdminWarnings.jsx
+++ Front/src/pages/Admin/Warnings/AdminWarnings.jsx
@@ -10,39 +10,36 @@ import { toast } from "react-toastify";
 function AdminWarnings() {
   const [warnings, setWarnings] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [noMoreItems, setNoMoreItems] = useState(false); // Estado para exibir a mensagem
+  const [noMoreItems, setNoMoreItems] = useState(false); 
   const [filters, setFilters] = useState({
     searchTerm: "",
     filterBy: "all",
     sortBy: "title",
     sortOrder: "asc",
     currentPage: 1,
-    itemsPerPage: 5, // Inicialmente 5 itens por página
+    itemsPerPage: 5, 
   });
 
-  // Busca os avisos conforme os filtros atuais
   const fetchWarnings = async () => {
     try {
       const response = await api.get(`/message?page=${filters.currentPage}&limit=${filters.itemsPerPage}`);
 
       if (response.data.length === 0) {
         if (!noMoreItems) {
-          setNoMoreItems(true); // Exibe a mensagem apenas na primeira vez
+          setNoMoreItems(true); 
           toast.info("Não há mais itens para exibir.");
         }
         setFilters((prev) => ({ ...prev, currentPage: prev.currentPage - 1 }));
         return;
       }
 
-      setNoMoreItems(false); // Reseta o aviso se encontrar itens
+      setNoMoreItems(false); 
       setWarnings(response.data);
     } catch (error) {
       console.error("Erro ao buscar as mensagens:", error);
       toast.error("Erro ao buscar as mensagens.");
     }
   };
-
-  // Refaz a busca sempre que `filters` mudar
   useEffect(() => {
     fetchWarnings();
   }, [filters.currentPage, filters.itemsPerPage]);
