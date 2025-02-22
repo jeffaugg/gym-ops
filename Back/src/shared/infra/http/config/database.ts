0064d31f348524e1e2207adbd1eca28a59e69579
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-02-22 12:17:19 -0300

    refactor: implementa padrão singleton para a instância do Knex no banco de dados

--- Back/src/shared/infra/http/config/database.ts
+++ Back/src/shared/infra/http/config/database.ts
@@ -1,8 +1,18 @@
 import knex, { Knex } from "knex";
 import config from "../../../../../knexfile";
 import { container } from "tsyringe";
-// Inicializa o Knex com o ambiente de desenvolvimento
-const db = knex(config.development);
-container.registerInstance<Knex>("Database", db);
+class Database {
+  private static instance: Knex;
 
-export default db;
+  private constructor() {}
+
+  public static getInstance(): Knex {
+    if (!Database.instance) {
+      Database.instance = knex(config.development);
+    }
+    return Database.instance;
+  }
+}
+container.registerInstance<Knex>("Database", Database.getInstance());
+
+export default Database.getInstance();
