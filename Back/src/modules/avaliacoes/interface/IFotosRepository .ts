4d225ce024d8027cf59c99bd1a50115dfa54e9b0
jeffaugg <jefersonaugusto@alu.ufc.br>
2025-02-22 20:21:08 -0300

    feat: adiciona método para atualizar fotos associadas a uma avaliação

--- Back/src/modules/avaliacoes/interface/IFotosRepository .ts
+++ Back/src/modules/avaliacoes/interface/IFotosRepository .ts
@@ -1,4 +1,5 @@
 export interface IFotosRepository {
   create(url: string, avaliacao_id: number): Promise<string>;
   findByAvaliacaoId(avaliacao_id: number): Promise<string[]>;
+  updatePhotos(avaliacao_id: number, fotos: string[]): Promise<string[]>;
 }
