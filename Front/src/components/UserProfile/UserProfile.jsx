cc4ec26f31ef44523961102d25ff13bff3eabb44
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2025-02-22 21:17:28 -0300

    fix: remove avatar prop from UserProfile component and update default avatar image

--- Front/src/components/UserProfile/UserProfile.jsx
+++ Front/src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./UserProfile.css";
 
-function UserProfile({ name, email, avatar }) {
+function UserProfile({ name, email }) {
   return (
     <div className="user-profile">
-      <img src="https://via.placeholder.com/50" alt="User Avatar" className="avatar" />
+      <img src="https://thumbs.dreamstime.com/b/linha-%C3%ADcone-do-preto-avatar-perfil-de-usu%C3%A1rio-121102131.jpg" alt="User Avatar" className="avatar" />
       <div className="user-info">
         <h4>{name}</h4>
         <p>{email}</p>
