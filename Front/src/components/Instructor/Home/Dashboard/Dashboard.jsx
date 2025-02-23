a2bb0cc16d6173fad447c159dbea3df59e30dbf1
Guilherme Pereira <guilhermepereira@alu.ufc.br>
2025-02-22 22:26:32 -0300

    feat: adiciona dados recentes de frequência e registros ao Dashboard e atualiza o componente DashboardCard

--- Front/src/components/Instructor/Home/Dashboard/Dashboard.jsx
+++ Front/src/components/Instructor/Home/Dashboard/Dashboard.jsx
@@ -1,23 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardCard from "./DashboardCard";
-import TopFrequentUsers from "../TopFrequentUsers/TopFrequentUsers";
+import TopFrequentUsers from "../../../Admin/Home/TopFrequentUsers/TopFrequentUsers";
+import api from "../../../../api";
 import "./Dashboard.css";
 
 function Dashboard() {
+  const [recentFrequencyData, setRecentFrequencyData] = useState([]);
+  const [recentRecordsData, setRecentRecordsData] = useState([]);
+  const [instructorsNow, setInstructorsNowData] = useState([]);
+  const [ranked, setRanked] = useState([]); 
+
+  const fetchRecentFrequency = async () => {
+    try {
+      const response = await api.get("/report/recent-frequency", {
+        params: {
+          limit: 3, 
+        }
+      });
+      setRecentFrequencyData(response.data); 
+    } catch (error) {
+      console.error("Erro ao buscar dados de frequência", error);
+    }
+  };
+  const fetchRecentRecords = async () => {
+    try {
+      const response = await api.get("/report/recent", {
+        params: {
+          limit: 3,  
+        }
+      });
+      setRecentRecordsData(response.data); 
+    } catch (error) {
+      console.error("Erro ao buscar dados de frequência", error);
+    }
+  };
+  const fetchInstructorsNow = async () => {
+    try {
+      const response = await api.get("/report/now", {
+        params: {
+          limit: 3,  
+        }
+      });
+      setInstructorsNowData(response.data); 
+    } catch (error) {
+      console.error("Erro ao buscar dados de frequência", error);
+    }
+  };
+  const fetchRanked = async () => {
+    try {
+      const response = await api.get("/report/rank", {
+        params: {
+          limit: 5,  
+        }
+      });
+      setRanked(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar dados de frequência", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecentFrequency();
+    fetchRecentRecords();
+    fetchInstructorsNow();
+    fetchRanked();
+  }, []); 
+
   return (
     <div className="dashboard">
       <div className="dashboard-header">
-        <DashboardCard title="Últimos registros" items={["Juan Dela Cruz", "Juan Dela Cruz", "Juan Dela Cruz"]} />
-        <DashboardCard title="Últimos alunos" items={["Juan Dela Cruz", "Juan Dela Cruz", "Juan Dela Cruz"]} />
-      </div>
 
-      <div className="dashboard-stats">
         
+        <DashboardCard title="Últimos registros" items={recentFrequencyData.map((item) => item.name)} />
+        <DashboardCard title="Últimos alunos" items={recentRecordsData.map((item) => item.name)} />
       </div>
 
-      <TopFrequentUsers />
+      <TopFrequentUsers users={ranked}/>
     </div>
   );
 }
+
 
 export default Dashboard;
